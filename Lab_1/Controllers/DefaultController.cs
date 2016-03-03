987767fd3bc5aa96033aae517830f6d2cccef3ca
m.sadofyev <m.sadofyev@infinnity.ru>
2016-03-03 20:04:30 +0500

    Add Lab 1

---
+++ Lab_1/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private static readonly string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "data.txt");
+
+        public ActionResult Index(string line)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                System.IO.File.AppendAllText(file, $"{line}{Environment.NewLine}");
+            }
+
+            return View();
+        }
+
+        public ActionResult All()
+        {
+            ViewBag.Lines = System.IO.File.ReadAllLines(file);
+
+            return View();
+        }
+    }
+}
