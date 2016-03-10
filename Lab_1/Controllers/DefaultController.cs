9bf94c5c3fc3d0f00372b3fd398e5e3b6687a124
m.sadofyev <m.sadofyev@infinnity.ru>
2016-03-10 08:59:41 +0500

    Update lab 1

--- Lab_1/Controllers/DefaultController.cs
+++ Lab_1/Controllers/DefaultController.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.IO;
 using System.Web.Mvc;
 
 namespace Lab_1.Controllers
 {
     public class DefaultController : Controller
     {
-        private static readonly string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "data.txt");
-
         public ActionResult Index(string line)
         {
+            var file = Server.MapPath("~/App_Data/data.txt");
+
             if (!string.IsNullOrWhiteSpace(line))
             {
                 System.IO.File.AppendAllText(file, $"{line}{Environment.NewLine}");
@@ -20,6 +19,8 @@ namespace Lab_1.Controllers
 
         public ActionResult All()
         {
+            var file = Server.MapPath("~/App_Data/data.txt");
+
             ViewBag.Lines = System.IO.File.ReadAllLines(file);
 
             return View();
